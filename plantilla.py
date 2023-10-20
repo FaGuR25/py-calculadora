3641683c0001ff32aa31c5ca2b01d1f1b9828c4f
chemabazan78@gmail.com <chemabazan78@gmail.com>
2023-10-20 12:00:36 -0600

    funcion modificada

--- plantilla.py
+++
@@ -1,5 +0,0 @@
-def funcname(num1:float, num2:float)->float:
- 
-    suma=num1+num2
-    return suma
-    pass
---
+++ suma.py
@@ -0,0 +1,5 @@
+def funcname(num1:float, num2:float)->float:
+ 
+    suma=num1+num2
+    return suma                                                                     
+    pass    
