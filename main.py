0011e095591e7a3153daf466f59a6381fac175d0
TDTxLE <tdtxle@gmail.com>
2023-10-20 12:17:38 -0600

    acomodo

--- main.py
+++ main.py
@@ -14,35 +14,55 @@ def Menu():
         print("Que operacion deseas realizar?\n1) suma\n2) resta\n3) multiplicacion\n4) divicion")
         accion = input("<< ")
 
-        num1 = float(input("Ingrese el primer valor: "))
-        num2 = float(input("Ingrese el segundo valor: "))
+        if accion =='':
+            os.system('cls')
+            print("Programa terminado")
+            break;        
 
         if accion == '1':
             os.system('cls')
+            num1 = float(input("Ingrese el primer valor: "))
+            num2 = float(input("Ingrese el segundo valor: "))
+
             res = suma(num1=num1,num2=num2)
             print(f"El resultado es: {res}")
+            
             input()
         elif accion =='2':
             os.system('cls')
+            num1 = float(input("Ingrese el primer valor: "))
+            num2 = float(input("Ingrese el segundo valor: "))
+
             res = resta(num1=num1,num2=num2)
             print(f"El resultado es: {res}")
+            
             input()
         elif accion =='3':
             os.system('cls')
+            num1 = float(input("Ingrese el primer valor: "))
+            num2 = float(input("Ingrese el segundo valor: "))
+
             res = multiplicacion(num1=num1,num2=num2)
             print(f"El resultado es: {res}")
+            
             input()
 
         elif accion =='4':
             os.system('cls')
+            num1 = float(input("Ingrese el primer valor: "))
+            num2 = float(input("Ingrese el segundo valor: "))
+
             res = division(num1=num1,num2=num2)
             print(f"El resultado es: {res}")
+            
             input()
-
         else:
             os.system('cls')
-            print("Programa terminado")
-            break;
+
+            print("Opcion no valida")
+            input()
+            
+
 
 
 if __name__ == "__main__":
