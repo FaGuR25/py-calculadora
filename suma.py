a8b852c1a8c675c331ce052295a9fea3b012b959
chemabazan78@gmail.com <chemabazan78@gmail.com>
2023-10-20 12:06:07 -0600

    funcion corregida

--- suma.py
+++ suma.py
@@ -1,4 +1,4 @@
-def funcname(num1:float, num2:float)->float:
+def suma(num1:float, num2:float)->float:
  
     suma=num1+num2
     return suma                                                                     
